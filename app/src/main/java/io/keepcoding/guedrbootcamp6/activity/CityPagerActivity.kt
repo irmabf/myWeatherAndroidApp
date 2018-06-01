957d381a8356ad55a0df577fce4e3d9d83b4e718
Irma <irblancofdz@gmail.com>
2018-06-01 20:29:13 +0200

    City name is shown on Toolbar

--- app/src/main/java/io/keepcoding/guedrbootcamp6/activity/CityPagerActivity.kt
+++ app/src/main/java/io/keepcoding/guedrbootcamp6/activity/CityPagerActivity.kt
@@ -4,24 +4,25 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.Menu
 import android.view.MenuItem
 import io.keepcoding.guedrbootcamp6.R
 import io.keepcoding.guedrbootcamp6.fragment.ForecastFragment
 import io.keepcoding.guedrbootcamp6.model.Cities
 import kotlinx.android.synthetic.main.activity_city_pager.*
+import java.text.FieldPosition
 
 class CityPagerActivity : AppCompatActivity() {
 
+    private val cities = Cities()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city_pager)
 
         toolbar.setLogo(R.mipmap.ic_launcher)
         setSupportActionBar(toolbar)
-
-
-        val cities = Cities()
 
         val adapter = object: FragmentPagerAdapter(supportFragmentManager) {
             override fun getItem(position: Int): Fragment {
@@ -37,6 +38,24 @@ class CityPagerActivity : AppCompatActivity() {
         }
 
         view_pager.adapter = adapter
+        view_pager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {}
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+            override fun onPageSelected(position: Int) {
+                updateCityInfo(position)
+            }
+        })
+
+        updateCityInfo(0)
+
+        //Añadir listener al view pager para cuando el usuario cambia de opcion en el menu
+
+    }
+
+    private fun updateCityInfo(position: Int){
+       supportActionBar?.title = cities.getCity(position).name
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
